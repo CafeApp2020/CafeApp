1f32847a24a8fe83c1b6d5eae1a3fc5795fe7d66
Oleg <49286750+Daganep@users.noreply.github.com>
2020-11-17 08:06:24 +0300

    task 004 & task 008. 1. LinearLayout в category_view_holder заменён на FrameLayout. 2. Отступы из category_view_holder перенесены в dimens. 3. Название recyclerView заменено на categoryListRV. 4. Удалён закомментированный код и логи не относящиеся к проекту. 5. Строки перенесены в ресурсы. 6. CategoryListRVAdapter перенесён в пакет к соответствующему фрагменту. 7. Добавлены недостающие модули в startkoin. 8. Поправлены конструкторы классов - теперь принимают интерфейс ICategoryInteractor.

--- app/src/main/java/com/cafeapp/mycafe/frameworks/view/categorylist/CategoryListFragment.kt
+++ app/src/main/java/com/cafeapp/mycafe/frameworks/view/categorylist/CategoryListFragment.kt
@@ -5,23 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.cafeapp.mycafe.R
-import com.cafeapp.mycafe.interface_adapters.adapters.CategoryListRVAdapter
 import com.cafeapp.mycafe.interface_adapters.viewmodels.categories.CategoryListViewModel
-import com.cafeapp.mycafe.use_case.utils.MsgState
-import com.cafeapp.mycafe.use_case.utils.SharedMsg
 import com.cafeapp.mycafe.use_case.utils.SharedViewModel
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.less.repository.db.room.DishesEntity
 import kotlinx.android.synthetic.main.fragment_categorylist.view.*
-import kotlinx.android.synthetic.main.fragment_dishesadd.*
+import org.koin.androidx.scope.currentScope
 
 // // Экран для отображения категорий блюд
 class CategoryListFragment : Fragment() {
-    private lateinit var categoryListViewModel: CategoryListViewModel
+    private val categoryListViewModel: CategoryListViewModel by currentScope.inject()
     private lateinit var adapter: CategoryListRVAdapter
 
     private val sharedModel by lazy {
@@ -36,14 +30,14 @@ class CategoryListFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_categorylist, container, false)
         adapter = CategoryListRVAdapter()
 
-        /*categoryListViewModel.categoryViewState.observe(viewLifecycleOwner, {
+        categoryListViewModel.categoryViewState.observe(viewLifecycleOwner, {
             if(it.categories != null){
                 adapter.data = it.categories
             }
-        })*/
+        })
 
-        root.recyclerView.layoutManager = GridLayoutManager(activity, 2)
-        root.recyclerView.adapter = adapter
+        root.categoryListRV.layoutManager = GridLayoutManager(activity, 2)
+        root.categoryListRV.adapter = adapter
 
         return root
     }
--- app/src/main/java/com/cafeapp/mycafe/interface_adapters/viewmodels/categories/CategoryAddViewModel.kt
+++ app/src/main/java/com/cafeapp/mycafe/interface_adapters/viewmodels/categories/CategoryAddViewModel.kt
@@ -3,13 +3,13 @@ package com.cafeapp.mycafe.interface_adapters.viewmodels.categories
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.cafeapp.mycafe.use_case.interactors.categories.CategoryInteractor
+import com.cafeapp.mycafe.use_case.interactors.categories.ICategoryInteractor
 import com.less.repository.db.room.CategoryEntity
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
-class CategoryAddViewModel(val categoryInteractor: CategoryInteractor) : ViewModel() {
+class CategoryAddViewModel(val categoryInteractor: ICategoryInteractor) : ViewModel() {
     private val modifyCategoryViewState = MutableLiveData<CategoryAddViewState>()
     val categoryViewState: LiveData<CategoryAddViewState> = modifyCategoryViewState
     val compositeDisposable: CompositeDisposable = CompositeDisposable()
